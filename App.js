bc92c0d754199780ef3b554f4514fd7a72997146
miha53cevic <44951515+miha53cevic@users.noreply.github.com>
2021-03-25 11:20:29 +0100

    initial

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
+
+import * as MediaLibrary from 'expo-media-library';
+import { Audio } from 'expo-av';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [sound, setSound] = useState();
+
+    const AskForPermission = async () => {
+        const permission = await MediaLibrary.requestPermissionsAsync();
+        if (!permission.granted) {
+            console.error("Missing permissions!");
+            return false;
+        } else return true;
+    };
+
+    const prepareAudio = async (file) => {
+        const perm = await AskForPermission();
+        if (perm) {
+            const { sound } = await Audio.Sound.createAsync(
+                { uri: file },
+                { shouldPlay: false }
+            );
+            
+            setSound(sound);
+        } else console.error("Could not prepareAudio!");
+    };
+
+    React.useEffect(() => {
+        return sound
+          ? () => {
+              console.log('Unloading Sound');
+              sound.unloadAsync(); }
+          : undefined;
+      }, [sound]);
+
+    const PlayAudio = async () => {
+        if (sound._loaded) {
+            const play = await sound.playAsync();
+        } else console.log("Sound has not been loaded yet");
+    }
+
+    const PauseAudio = async () => {
+        const pause = await sound.pauseAsync();
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text>Hello World</Text>
+            <Button onPress={() => prepareAudio("file:///storage/emulated/0/Music/Over the Horizon.mp3")} title='LoadSong'></Button>
+            <Button onPress={() => PlayAudio()} title='Play'></Button>
+            <Button onPress={() => PauseAudio()} title='Pause'></Button>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
