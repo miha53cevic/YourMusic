8afc0b86d2b31be70754ecfa7dc672ba1c6ea30a
miha53cevic <44951515+miha53cevic@users.noreply.github.com>
2021-10-30 17:25:04 +0200

    fix react error

--- src/App.js
+++ src/App.js
@@ -29,23 +29,27 @@ export default function App() {
 
     // Initialize TrackPlayer
     useEffect(async () => {
-        await TrackPlayer.setupPlayer();
+        async function setupTrackPlayer() {
+            await TrackPlayer.setupPlayer();
 
-        // Media controls for background
-        await TrackPlayer.updateOptions({
-            stopWithApp: true,
-            capabilities: [
-                TrackPlayer.CAPABILITY_PLAY,
-                TrackPlayer.CAPABILITY_PAUSE,
-                TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
-                TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
-                TrackPlayer.CAPABILITY_STOP,
-            ],
-            compactCapabilities: [
-                TrackPlayer.CAPABILITY_PLAY,
-                TrackPlayer.CAPABILITY_PAUSE
-            ]
-        });
+            // Media controls for background
+            await TrackPlayer.updateOptions({
+                stopWithApp: true,
+                capabilities: [
+                    TrackPlayer.CAPABILITY_PLAY,
+                    TrackPlayer.CAPABILITY_PAUSE,
+                    TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
+                    TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
+                    TrackPlayer.CAPABILITY_STOP,
+                ],
+                compactCapabilities: [
+                    TrackPlayer.CAPABILITY_PLAY,
+                    TrackPlayer.CAPABILITY_PAUSE
+                ]
+            });
+        }
+        
+        setupTrackPlayer();
     }, []);
 
     /////////////////////////////////////////////////////////////////////////////////////////////
